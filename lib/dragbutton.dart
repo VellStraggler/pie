d10b978c934aceee9026e58fedb2ece269403484
VellStraggler <david@incwells.com>
2024-10-09 19:20:29 -0600

    small main changes

--- lib/dragbutton.dart
+++ lib/dragbutton.dart
@@ -1,14 +1,14 @@
-<<<<<<< HEAD
-class MyClass {
+//<<<<<<< HEAD
+//class MyClass {
   // This will be found on the corners of every task (except the center one)
   // It is meant to show up when in edit mode or zoomed in to level 3
   // It is used to drag a task's starting or ending point
 
-    String name;
-    int age;
+//    String name;
+//    int age;
 //=======
 //linden jensen
->>>>>>> e09481706d681936c83085987d32d3986f100f62
+//>>>>>>> e09481706d681936c83085987d32d3986f100f62
 
 class DragButton {
   Point point;           
@@ -23,3 +23,4 @@ class DragButton {
   void dragTo(Point newPoint) {
     this.point = newPoint;
   }
+}
