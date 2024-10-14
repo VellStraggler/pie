3e53d4fd61d2dcf81d23fe3ab176ae091bfb27ac
JoshZ520 <joshuazob@gmail.com>
2024-10-14 11:44:41 -0600

    just added a thing to draw the polygons

--- lib/polygon.dart
+++ lib/polygon.dart
@@ -1,4 +1,6 @@
 // Josh Zobrist is working on this
+import 'package:flutter/material.dart';
+
 class Polygon {
   // THIS IS for making classes with GRAPHICS
   String name;
@@ -8,5 +10,15 @@ class Polygon {
 
   void displayInfo() {
     print('Name: $name, Age: $age');
+  }
+
+  Widget draw() {
+    return Container(
+      width: 50,
+      height: 100,
+      color: Colors.amber,
+      child:
+          Center(child: Text('$name', style: TextStyle(color: Colors.white))),
+    );
   }
 }
