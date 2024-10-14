d8dd790854d19ec10033ce7954e0a3ddcd8a9d0a
VellStraggler <david@incwells.com>
2024-10-14 14:47:55 -0600

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:pie_agenda/pie.dart';
+import 'package:pie_agenda/polygon.dart';
+import 'package:pie_agenda/slice.dart';
+import 'package:pie_agenda/task.dart';
+import 'Point.dart';
 
 void main() {
+  // Initialize PM Pie, AM Pie is for later
+  List<Slice> slices = [];
+  Point center = Point(800,800);
+  Polygon circle = Polygon()
+  Pie pie = Pie(slices: slices, center: center, circle: circle);
+
+  int zoom = 1; //zoom range from 1 to 3
+
+
   runApp(const MyApp());
 }
+void addTask(Task task) {
 
+}
+void removeTask(Task task) {
+
+}
+void zoom(bool up) {
+  //maintain range of 1 to 3
+}
 
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
 
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'pie-agenda',
       theme: ThemeData(
         // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
+        // press "r" in console to reload
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Pie Agenda Home Page'),
     );
   }
 }
@@ -64,15 +78,8 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _toggleEditMode method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
@@ -82,19 +89,6 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
               Text('You have turned on edit mode'),
@@ -103,7 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _toggleEditMode,
-        tooltip: 'Increment',
+        tooltip: 'Toggle',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
